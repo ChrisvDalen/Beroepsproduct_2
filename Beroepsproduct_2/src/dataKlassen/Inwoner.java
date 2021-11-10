59e162e351e9721d6cfc20a914bf653f4a526c9e
ChrisvDalen-cyber <82801071+ChrisvDalen-cyber@users.noreply.github.com>
2021-11-10 12:20:41 +0100

    Update 5
    - Added getters and setters to the "datum" and "inwoner" classes.

--- Beroepsproduct_2/src/dataKlassen/Inwoner.java
+++ Beroepsproduct_2/src/dataKlassen/Inwoner.java
@@ -2,6 +2,32 @@ package dataKlassen;
 
 public class Inwoner {
 	
+	private String naam;
+	private int leeftijd;
+	
+	public Inwoner() {
+		this.naam = naam;
+		this.leeftijd = leeftijd;
+		
+	}
+	
+	public void setNaam( ) {
+		this.naam = naam;
+	}
+	
+	public String getNaam() {
+		return naam;
+		
+	}
+	
+	public void setLeeftijd() {
+		this.leeftijd = leeftijd;
+	}
+	
+	public int getLeeftijd() {
+		return leeftijd;
+	}
+	
 	
 
 }
